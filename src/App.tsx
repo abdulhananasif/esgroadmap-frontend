d423c810fbafe9d5b742196f83e05801f32e5f1e
Junaid Saroya <junaidsaroya1611@gmail.com>
2025-04-23 11:07:04 +0500

    remove unused

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,6 @@
-import {useEffect} from 'react';
 import {AppRoutes} from './AppRoutes';
-import {isAuthenticated} from './utils/auth';
-import {useNavigate} from 'react-router-dom';
 
 const App = () => {
-  // const navigate = useNavigate();
-  // useEffect(() => {
-  //   if (isAuthenticated()) {
-  //     navigate('/dashboard');
-  //   } else {
-  //     navigate('/auth/login');
-  //   }
-  // }, []);
   return <AppRoutes />;
 };
 
